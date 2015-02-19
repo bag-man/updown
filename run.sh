3a6885139541165dd10957af51524867e30b4e4a
bag-man <garland.owen@gmail.com>
2015-02-19 11:27:47 -0500

    Added script

---
+++ run.sh
@@ -0,0 +1,6 @@
+while true; do
+  heroku ps:scale web=0
+  sleep 30
+  heroku ps:scale web=1
+  sleep 30
+done 
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
   app.set('port', (process.env.PORT || 5000));
   app.get('/', function(request, response) {
-    response.send('This website will go up and down every two minutes.');
+    response.send('This website will go up and down every 30 seconds.');
   });
   app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
