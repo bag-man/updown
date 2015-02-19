66fa37c2f2c1ecfc2f23da002d4c1a39576b6ecd
bag-man <garland.owen@gmail.com>
2015-02-19 11:20:01 -0500

    Updated

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('This website will go down every two minutes\n');
-}).listen(80, '127.0.0.1');
-console.log('Server running');
+var express = require('express');
+var app = express();
+  app.set('port', (process.env.PORT || 5000));
+  app.get('/', function(request, response) {
+    response.send('This website will go up and down every two minutes.');
+  });
+  app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
