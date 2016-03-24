47a26bc12774c7e14fafc6b62a3a86a409c161f5
bag-man <garland.owen@gmail.com>
2016-03-24 13:00:34 -0400

    > Added app arg

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 while true; do
-  heroku ps:scale web=0
+  heroku ps:scale web=0 --app upanddown
   sleep 30
-  heroku ps:scale web=1
+  heroku ps:scale web=1 --app upanddown
   sleep 30
 done 
 # nohup ./run.sh > /dev/null &
